90ac794ca60cbc913e1dd4fd3b36fed825b72031
anıl altan <32691333+anlaltan@users.noreply.github.com>
2020-12-07 00:31:54 +0300

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,8 @@
+<footer class="page-footer font-small blue">
+    <div class="footer-copyright text-center py-3">© 2020 Copyright:
+        <a href="https://mdbootstrap.com/"> MDBootstrap.com</a> by <a href="https://twitter.com/altn_anl">Anıl Altan</a>
+    </div>
+</footer>
+
+</body>
+</html>
